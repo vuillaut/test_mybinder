798395420f3363c2b439a0e610ff3ee8e511faec
vuillaut <thomas.vuillaume@gmail.com>
2022-10-18 01:02:38 +0200

    req.txt

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='testmybinder',
+    version='0.1',
     description="testmybinder",
     setup_requires="setuptools_scm",
     install_requires=[
